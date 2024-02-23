4e9618d399e8953f18eae71ee49ab79b54eff519
andy-cpp <andyaalgaard@gmail.com>
2024-02-23 07:51:57 +0100

    Updated main.cpp example

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,6 @@ int main()
 {
     Subprocess process;
     process.Exec("env");
-    Subprocess::Pipes const& pipes = process.GetPipes();
 
-    char buffer[32768] = {0};
-    read(pipes.Stdout[0], buffer, sizeof buffer);
-    printf("%s\n", buffer);
+    std::cout << process.Read() << "\n";
 }
